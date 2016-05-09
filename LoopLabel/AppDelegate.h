bff8f3a67d8e17ec0296be4dd5ddac38fd867548
wukongliang <wukongliang@58gxb.com>
2016-05-09 10:33:51 +0800

    Initial Commit

---
+++ LoopLabel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoopLabel
+//
+//  Created by Ray on 16/5/9.
+//  Copyright © 2016年 Ray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LoopLabel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoopLabel
+//
+//  Created by Ray on 16/5/9.
+//  Copyright © 2016年 Ray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
